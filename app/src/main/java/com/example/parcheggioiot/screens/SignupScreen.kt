dd1945124eeca08c2a2b49d7e7627dc357333425
rossosbiadito <168523942+rossosbiadito@users.noreply.github.com>
2026-05-17 18:19:16 +0200

    invio dati al database con aiuto raspberrybj

--- app/src/main/java/com/example/parcheggioiot/screens/SignupScreen.kt
+++ app/src/main/java/com/example/parcheggioiot/screens/SignupScreen.kt
@@ -1,5 +1,5 @@
 package com.example.parcheggioiot.screens
-// prova perchè github è storto! merda github
+// prova perchè github è storto! merda github nlkòn
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
